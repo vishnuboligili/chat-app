b80c9af45516e14a4a39f2bac23743ef6508228b
vishnuboligili <vishnuboligili@gmail.com>
2025-12-02 14:32:16 +0530

    Update axios baseURL for production environment

--- fontend/src/lib/axios.js
+++ fontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chat-app-skuv.onrender.com/api",
   withCredentials: true,
 });
