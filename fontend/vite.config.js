caf42f77525e2a0de72b218effd1a38b08c32713
vishnuboligili <vishnuboligili@gmail.com>
2025-12-02 16:44:59 +0530

    Update vite.config.js

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // important for Netlify
-});
+  base: './', // Important for relative paths on Netlify
+})
